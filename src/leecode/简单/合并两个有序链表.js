7a44250c1039c47345813861a16693c6f3a9a81c
gerry <957282514@qq.com>
2021-12-05 21:21:05 +0800

    modify

--- src/leecode/简单/合并两个有序链表.js
+++ src/leecode/简单/合并两个有序链表.js
@@ -11,5 +11,31 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-
+    if (!list1.next) return list2
+    if (!list2.next) return list1
+    // 新建一个空的ListNode
+    let newList = new ListNode()
+    let item1 = list1
+    let item2 = list2
+    while (list1.next || list2.next) {
+        if (item1 > item2) {
+            newList.next = new ListNode(item2)
+            item2 = list2.next
+        } else {
+            newList.next = new ListNode(item1)
+            item1 = list1.next
+        }
+    }
+    return newList
 };
+/**
+ * @description JavaScript实现单向链表
+ * @param {string} val 
+ * @param {ListNode} next 
+ */
+function ListNode (val, next) {
+    this.val = (val=== undefined ? 0 : val)
+    this.next = (next=== undefined ? null : next)
+}
+let l1 = [1,2,4], l2 = [1,3,4]
+console.log(mergeTwoLists(l1, l2))
